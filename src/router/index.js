4e33b08330ac374bd3812952b86121c2f52270f8
jayjain2 <jjain654@gmail.com>
2020-08-03 11:34:34 -0500

    Added ForgotPassword

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,16 @@ Vue.use(VueRouter)
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/auth/Login.vue')
     }
+  },
+  {
+    path: '/forgot-password',
+    name: 'ForgotPassword',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ '../views/auth/ForgotPassword.vue')
+    }
   }
 ]
 
---
+++ src/views/auth/ForgotPassword.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <form>
+            <h3>Forgot Password</h3>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+
+            <button type="submit" class="btn btn-primary btn-lg btn-block">Reset password</button>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
